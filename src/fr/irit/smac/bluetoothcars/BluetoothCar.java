8693ac0ad2210c98a2409dae15970373fc85ceb0
shino <shino@141.115.39.101>
2020-08-03 11:12:45 +0200

    Commit test

--- src/fr/irit/smac/bluetoothcars/BluetoothCar.java
+++ src/fr/irit/smac/bluetoothcars/BluetoothCar.java
@@ -108,6 +108,7 @@ public class BluetoothCar {
 		}
 	}
 
+	/* macro */
 	public void m2() {
 		try {
 			forwardToRight();
--- src/fr/irit/smac/bluetoothclient/BluetoothClient.java
+++ src/fr/irit/smac/bluetoothclient/BluetoothClient.java
@@ -33,6 +33,7 @@ public class BluetoothClient {
 		}
 	}
 
+	/* find */
 	public void findDevices() {
 		try {
 			discoveryAgent.startInquiry(DiscoveryAgent.GIAC, listener);
